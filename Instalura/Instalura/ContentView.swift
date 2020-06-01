95d6deb716b43f44beedd4505cccbef816cdd55e
CarlosAmorim <carlos.amorim@rckyinformatica.onmicrosoft.com>
2020-06-01 17:35:40 -0300

    projeto ios

---
+++ Instalura/Instalura/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instalura
+//
+//  Created by DesenvolvedorApp_RCKY on 01/06/20.
+//  Copyright © 2020 DesenvolvedorApp_RCKY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
